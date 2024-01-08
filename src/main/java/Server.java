68d7ddd3b9f336bdb1180b5b04097a5bc461b9b8
JamesDex <cowsert.jamesh@gmail.com>
2024-01-08 02:40:17 -0600

    Server stuff

--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -1,2 +1,37 @@
-public class Server {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.*;
+public class Server implements Runnable {
+
+    @Override
+    public void run() {
+        try {
+            ServerSocket server = new ServerSocket(5001);
+            Socket client = server.accept();
+        } catch (IOException e) {
+            // TODO: handle
+        }
+    }
+
+    class ConnectionHandler implements Runnable {
+        private Socket client;
+        private BufferedReader input;
+        private PrintWriter output;
+
+        public ConnectionHandler(Socket client) {
+            this.client = client;
+        }
+        @Override
+        public void run() {
+            try {
+                output = new PrintWriter(client.getOutputStream(), true);
+                input = new BufferedReader(new InputStreamReader(client.getInputStream()));
+            } catch (IOException e) {
+                // TODO: handle
+            }
+
+        }
+    }
 }
