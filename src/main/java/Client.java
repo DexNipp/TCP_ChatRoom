c02cf50ab21d65be47d038673742e6036bbc7a04
JamesDex <cowsert.jamesh@gmail.com>
2024-01-10 19:25:27 -0600

    Server stuff

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -1,3 +1,66 @@
-public class Client {
+import java.io.*;
+import java.net.*;
 
+public class Client implements Runnable {
+    private Socket client;
+    private BufferedReader input;
+    private PrintWriter output;
+    private boolean done;
+    @Override
+    public void run() {
+        try {
+            Socket client = new Socket("127.0.0.1", 5001);
+            output = new PrintWriter(client.getOutputStream(), true);
+            input = new BufferedReader(new InputStreamReader(client.getInputStream()));
+
+            InputHandler inputHandler = new InputHandler();
+            Thread thread = new Thread(inputHandler);
+            thread.start();
+
+            String inputMessage;
+            while ((inputMessage = input.readLine()) != null) {
+                System.out.println(inputMessage);
+            }
+        } catch (IOException ex) {
+            shutdown();
+        }
+    }
+
+    public void shutdown() {
+        done = true;
+        try {
+            input.close();
+            output.close();
+            if (!client.isClosed()) {
+                client.close();
+            }
+        } catch (IOException ex) {
+            // ignore
+        }
+    }
+
+    class InputHandler implements Runnable {
+        @Override
+        public void run() {
+            try {
+                BufferedReader inputReader = new BufferedReader(new InputStreamReader(System.in));
+                while (!done) {
+                    String message = inputReader.readLine();
+                    if (message.equals("/quit")) {
+                        inputReader.close();
+                        shutdown();
+                    } else {
+                        output.println(message);
+                    }
+                }
+            } catch (IOException ex) {
+                shutdown();
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Client client = new Client();
+        client.run();
+    }
 }
--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -3,15 +3,56 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.*;
+import java.util.ArrayList;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class Server implements Runnable {
+
+    private ArrayList<ConnectionHandler> connections;
+    private ServerSocket server;
+    private boolean done;
+    private ExecutorService threadPool;
+    public Server() {
+        connections = new ArrayList<>();
+        done = false;
+    }
 
     @Override
     public void run() {
         try {
-            ServerSocket server = new ServerSocket(5001);
-            Socket client = server.accept();
-        } catch (IOException e) {
-            // TODO: handle
+            server = new ServerSocket(5001);
+            threadPool = Executors.newCachedThreadPool();
+            while (!done) {
+                Socket client = server.accept();
+                ConnectionHandler handler = new ConnectionHandler(client);
+                connections.add(handler);
+                threadPool.execute(handler);
+            }
+
+        } catch (Exception e) {
+            shutdown();
+        }
+    }
+
+    public void broadcast(String message) {
+        for (ConnectionHandler ch : connections) {
+            if (ch != null) {
+                ch.sendMessage(message);
+            }
+        }
+    }
+
+    public void shutdown() {
+        try {
+            if (!server.isClosed()) {
+                server.close();
+            }
+            for (ConnectionHandler ch : connections) {
+                ch.shutdown();
+            }
+        } catch (IOException exception) {
+            // ignore
         }
     }
 
@@ -19,6 +60,7 @@ public class Server implements Runnable {
         private Socket client;
         private BufferedReader input;
         private PrintWriter output;
+        private String nickname;
 
         public ConnectionHandler(Socket client) {
             this.client = client;
@@ -28,10 +70,51 @@ public class Server implements Runnable {
             try {
                 output = new PrintWriter(client.getOutputStream(), true);
                 input = new BufferedReader(new InputStreamReader(client.getInputStream()));
+
+                output.println("Enter a nickname: ");
+                nickname = input.readLine();
+
+                String message;
+                while ((message = input.readLine()) != null) {
+                    if (message.startsWith("/nickname ")) {
+                        String[] messageSplit = message.split(" ", 2);
+                        if (messageSplit.length == 2) {
+                            broadcast(nickname + " changed name to: " + messageSplit[1]);
+                            nickname = messageSplit[1];
+                        }
+                    }
+                    else if (message.startsWith("/quit")) {
+                        broadcast(nickname + " has left the chat.");
+                    } else {
+                        broadcast(nickname + ": " + message);
+                    }
+                }
+
             } catch (IOException e) {
-                // TODO: handle
+                shutdown();
             }
 
         }
+
+        public void sendMessage(String message) {
+            output.println(message);
+        }
+
+        public void shutdown() {
+            try {
+                input.close();
+                output.close();
+                if (!client.isClosed()) {
+                    client.close();
+                }
+            } catch (IOException ex) {
+                //ignore
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Server server = new Server();
+        server.run();
     }
 }
